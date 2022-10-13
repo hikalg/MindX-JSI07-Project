5a01a35191c8de9e592cd552d7896d4a0867168b
Hika <nam.luong2511@gmail.com>
2022-10-13 10:53:55 +0700

    added bootstrap, js details
    added text

---
+++ scripts/products.js
@@ -0,0 +1,57 @@
+const le_product_0 = {
+    name: "iPhone 14 Pro Max Christmas LE Red",
+    brand: "Apple",
+    id: 100, // mã sản phẩm
+    color: "red",
+    availability: "Limited+ (25)", // còn hàng hay không
+    desc1: "256GB", // mô tả chung
+    img1: "",
+    img2: "",
+}
+
+const le_product_1 = {
+    name: "PlayStation 5 Christmas LE",
+    brand: "Sony",
+    id: 101, // mã sản phẩm
+    color: "red / green",
+    availability: "Limited+ (10)", // còn hàng hay không
+    desc1: "Disc Edition", // mô tả chung
+    img1: "",
+    img2: "",
+}
+
+const le_product_2 = {
+    name: "ASUS ROG Zephyrus G14 Fusion Edition GA402RK-FS",
+    brand: "ASUS",
+    id: 102, // mã sản phẩm
+    color: "black / white",
+    availability: "Limited (100)", // còn hàng hay không
+    desc1: '14" ROG Nebula Display, 120hz AdaptiveSync, 16:10, Ryzen 9 6900HS, RX 6800S, 32GB DDR5 RAM, 1TB PCIe 4 NVMe SSD', // mô tả chung
+    img1: "",
+    img2: "",
+}
+
+const le_product_3 = {
+    name: "RTX 4090 FE Christmas RGB",
+    brand: "Nvidia",
+    id: 103,
+    color: "black",
+    availability: "Limited (10)",
+    desc1: "with OpenRGB support",
+    img1: "",
+    img2: "",
+}
+
+const le_product_4 = {
+    name: "Galaxy Z Flip4 Christmas Bespoke",
+    brand: "Samsung",
+    id: 104,
+    color: "red / green / black",
+    availability: "Limited (100)",
+    desc1: " ",
+    img1: "",
+    img2: "",
+}
+
+
+const le_products = [le_product_0, le_product_1, le_product_2, le_product_3, le_product_4]
